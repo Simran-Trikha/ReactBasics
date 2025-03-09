7b9d9cdcd3d96ee897ebd8cb0f59b9954f24a916
Simran-Trikha <simmitrikha97@gmail.com>
2025-03-09 14:55:56 +0530

    React Bundler - Parcel

--- App.js
+++ App.js
@@ -7,30 +7,29 @@
     </div>
 
 */
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+import React from "react";
+import { createRoot } from "react-dom/client"; // Correct import
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "h1",
-      { id: "heading" },
-      "Hello World from nested divs"
-    ),
-    React.createElement(
-      "h1",
-      { id: "heading" },
-      "Hello World from nested divs"
-    ),
+    React.createElement("h1", { id: "heading" }, "Hello World from 1 nested divs"),
+    React.createElement("h1", { id: "heading" }, "Hello World from 2 nested divs"),
   ])
 );
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement); // Use createRoot correctly
 root.render(parent);
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React!"
-);
+
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World from React!"
+// );
 
 // root.render(heading);
 
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ReactBasics
+
+
+# parcel
+    - making Dev Build
+    - Local Server
+    - HMR = HOT Module Replacement
+    - Automatically refreshes page 
+    - It is caching also - Faster Builds
+    - Image Optimization
+    - Minification of files
+    - Bundling
+    - Compressing - removing whitlisting
+    - File watching algo - written in c++
+    - consistent hashing
+    - code splitting
+    - Diffrential Bundling - (support of older browser)
+    - Diagnostics
+    - Error Handling
+    - can host the apps to HTTPS
+    - Tree shaking - remove unused code
+    - different builds for dev & prod
+
+
+
